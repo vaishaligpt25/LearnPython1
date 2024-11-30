ba251aba93edb9278fd1cff040616090fcbaa937
vaishaligpt25 <vaishali.gpt25@gmail.com>
2024-12-01 01:32:46 +0530

    create method2 of pattern6

--- patterns/pattern6.py
+++ patterns/pattern6.py
@@ -20,7 +20,7 @@ n = 4
 
 """
 
-def show_pattern_for_n(n: int) -> None:
+def show_pattern_for_n_method1(n: int) -> None:
     print(f"The pattern for n={n} is:-")
     for i in range(1, n + 1):
         for j in range(1, i + 1):
@@ -33,10 +33,25 @@ def show_pattern_for_n(n: int) -> None:
 
     print()
 
+def show_pattern_for_n_method2(n: int) -> None:
+    print(f"The pattern for n={n} is:-")
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print("^", end="")
+        print()
+    for i in range(1, n):
+        for j in range(1, n - i + 1):
+            print("^", end="")
+        print()
+
+    print()
+
 def test_cases() -> None:
-    show_pattern_for_n(n=4)
-    show_pattern_for_n(n=1)
-    show_pattern_for_n(n=7)
+    show_pattern_for_n_method1(n=4)
+    show_pattern_for_n_method1(n=1)
+
+    show_pattern_for_n_method2(n=7)
+    show_pattern_for_n_method2(n=9)
 
 if __name__ == '__main__':
     test_cases()
