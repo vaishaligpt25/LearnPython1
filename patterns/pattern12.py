af9017d5c2f55d9bf709ee25ad2c7344fdd83132
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-01-07 18:55:07 +0000

    create pattern12

--- patterns/pattern12.py
+++ patterns/pattern12.py
@@ -1,6 +1,27 @@
 """
-
-
-
+1
+2
+3
+4
+5 4 3 2 1
 
 """
+
+def show_pattern_for_n(n: int) -> None:
+    print(f"Pattern for n = {n} is :")
+
+    for i in range(1, n + 1):
+        if i < n:
+             print(i)
+        else:
+         for j in range(n, 0, -1):
+             print(j, end=" ")
+         print()
+    print()
+
+def test_cases() -> None:
+    show_pattern_for_n(5)
+
+
+if __name__ == '__main__':
+    test_cases()
