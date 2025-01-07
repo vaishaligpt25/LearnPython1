6403ade60bd9d0501934c6b64fea61cac7b8e24f
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-01-07 13:48:49 +0000

    2_loops_lists_and_more_fundamentals

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ https://thepythoncodingbook.com/book-outline/
 chapter 1:
 https://thepythoncodingbook.com/1-getting-started-with-the-fundamentals-of-programming/
 
-
+chapter 2:
+https://thepythoncodingbook.com/2-loops-lists-more-fundamentals/
---
+++ the_python_coading_book/2_loops_lists_and_more_fundamentals.py
@@ -0,0 +1,58 @@
+import random
+
+'''for something in range(10):
+    print("Don't repeat yourself")
+
+for something in range(10):
+    print(something)
+    print("something")
+
+for something in range(10):
+   print("something")
+
+print("\n-----1-----\n")
+
+for something in range (10):
+    print(f"{something + 1}. Don't repeat yourself.")
+
+print("\n-----2-----\n")
+
+a_number = 0
+while a_number <= 20:
+    a_number = random.randint(1, 25)
+    print(f"This is the random number : {a_number}")
+
+print("You have find larger than 20")
+
+print("\n-----3-----\n")
+
+a = random.randint(1,10)
+if a == 5:
+    print("a is 5, after all")
+    print("Great news")
+print("Have a good day")
+
+print("\n-----4-----\n")
+
+a = random.randint(1,10)
+if a == 5:
+
+print("Have a good day")
+
+print("\n-----5-----\n")
+
+a = random.randint(1,20)
+b = random.randint(1,20)
+
+if a > 10:
+    print("a is a big number")
+    if b > 10:
+        print("b is a big number")'''
+
+
+print("\n-----6-----\n")
+
+Team_members = ["Harry", "Vikas", "Vivek", "Happy"]
+for x in Team_members:
+    print(f"Hello {x}.How are you?")
+
